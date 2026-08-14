bee1750851c92543e2fecf2d0555b2317cfc12ec
Max01112 <maxim.reancev@mail.ru>
2026-08-14 22:41:24 +0300

    Work in process

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,38 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.print("Hello World!");
+        System.out.print("Задание 1");
+        byte bait = 1;
+        short karatkli = 2;
+        int odin = 3;
+        long dlinna = 4;
+        float sem = 2.1F;
+        double shet = 3.44;
+        System.out.println("Значение переменной bait с типом byte равно " + bait);
+        System.out.println("Значение переменной karatkli с типом short равно " + karatkli);
+        System.out.println("Значение переменной odin типом int равно " + odin);
+        System.out.println("Значение переменной dlinna типом long равно " + dlinna);
+        System.out.println("Значение переменной sem с типом float равно " + sem);
+        System.out.println("Значение переменной shet с типом double равно " + shet);
+
+        System.out.println("Задание 2");
+        double number1 = 27.12;
+        long number2 = 987678965549L;
+        double number3 = 2.786;
+        short number4 = 569;
+        int number5 = -159;
+        int number6 = 27897;
+        byte number7 = 67;
+        System.out.print(number1 + " " + number2 + " " + number3 + " " + number4 + " " + number5 + " " + number6 + " " + number7);
+
+        System.out.println("Задание 3");
+        byte class1 = 23;
+        byte class2 = 27;
+        byte class3 = 30;
+        int otvet = 480/(class1 + class2 + class3);
+        System.out.print("На каждого ученика рассчитано " + otvet + " листов бумаги");
+
+        System.out.println("Задание 4");
 
         }
     }
